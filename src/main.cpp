634d99039373c41d9f9b0d56a4576847824418b3
Dylan Gentile <dylangentile@ymail.com>
2019-01-17 10:28:56 -0800

    buying/selling working

--- src/main.cpp
+++ src/main.cpp
@@ -5,23 +5,87 @@
 
 using namespace std;
 
+string retplumin(int x) {
+	if (x < 0) {
+		return to_string(x);
+	}
+	if (x > 0) {
+		return "+" + to_string(x);
+
+	}
+	if (x == 0) {
+		return "0";
+	}
+	return "0";
+}
+
+
 int main(int argc, char const *argv[])
 {
+	
 	int stockMax = 8;
+	double cash = 1000;
 	int *values = new int[stockMax];
 	int *newMods = new int[stockMax];
+	int *own = new int[stockMax];
 	srand(time(NULL));
 	for (int i = 0; i < stockMax; i++) {
 		values[i] = rand() % 1000;
 		newMods[i] = values[i];
+		own[i] = 0;
 	}
 	cout << "\nWelcome to stock game!\n";
+	cout << "CASH: $" << cash << "\n";
 
 	while (true) {
-		char input;
+		string input;
+		
 		cout << "GlarCorp\tMixeCorp\tSnowCorp\tJoesCorp\n0:$" << values[0] << "\t\t$1:" << values[1] << "\t\t$2:" << values[2] << "\t\t$3:" << values[3] << "\n";
 		cout << "MarkCorp\tJohnCorp\tListCorp\tCodeCorp\n4:$" << values[4] << "\t\t$5:" << values[5] << "\t\t$6:" << values[6] << "\t\t$7:" << values[7] << "\n\n\n";
-		cin >> input;
+		
+		while (true) {
+			cout << "\tCASH: $" << cash << "\n";
+			cin >> input;
+			if (input == "d") {
+				break;
+			}
+
+			int idtobuy = input[0] - '0';
+			int howmany = input[2] - '0';
+
+			//cout << idtobuy;
+			//cout << howmany;
+			
+			if (input[3] == '-') {
+				//selling function
+				if (own[idtobuy] >= howmany) {
+					own[idtobuy] -= howmany;
+					cash += values[idtobuy] * howmany;
+				}
+			}
+			else if (input[1] == ':') {
+				//buying function
+				if (idtobuy >= 0 && idtobuy <= 8) {
+					int cost = values[idtobuy] * (howmany);
+					if ((double)cost < cash) {
+						own[idtobuy] += howmany;
+						cash = cash - cost;
+						cout << "\nTransaction Completed. Type 'd' to move to the next day.\nNext Transaction:";
+					}
+					else {
+						cout << "\nYou're too poor!\n";
+					}
+
+				}
+				else {
+					cout << "\nWhat you're trying to buy doesn't exist!\n";
+				}
+			}
+			else {
+				cout << "\nImproper Formatting!!\n";
+			}
+			
+		}
 
 		for (int i = 0; i < stockMax; i++) {
 			int chance = rand() % 2;
@@ -32,11 +96,22 @@ int main(int argc, char const *argv[])
 				newMods[i] = rand() % 100;
 			}
 		}
-		cout << "0:$+" << newMods[0] << "\t1:$+" << newMods[1] << "\t2:$+" << newMods[2] << "\t3:$+" << newMods[3] << "\t4:$+" << newMods[4] << "\t5:$+" << newMods[5] << "\t6:$+" << newMods[6] << "\t7:$+" << newMods[7] << "\n\n\n";
+		for (int i = 0; i < stockMax; i++) {
+			cout << i << ":" << retplumin(newMods[i]) << "\t";
+		}
+		cout << '\n';
 		cin >> input;
 		for (int i = 0; i < stockMax; i++) {
 			values[i] += newMods[i];
 		}
+		cout << "\n\n";
+		cout << "CASH: $" << cash << "\nWorth: ";
+		int wt = 0;
+		for (int i = 0; i < stockMax; i++) {
+			wt += values[i] * own[i];
+			cout << i << ':' << own[i] << " + ";
+		}
+		cout << "0 = $" << wt << "\n";
 	}
 	return 0;
 }
